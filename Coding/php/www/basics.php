95e026956faf7c8233b39e7fe6f94459e48abcf1
hnodriturbo <hreidar1987@gmail.com>
2023-06-22 10:46:57 +0000

    learning php basics

--- Coding/php/www/basics.php
+++ Coding/php/www/basics.php
@@ -23,21 +23,57 @@
         echo "and he didn't like to be named $name <br>";
         echo "<br><br>";
         echo "His age is $age";
+        echo "<br>";
 
         $phrase = "To be or not to be";
+        echo "<br>";
         $age = 30;
         $float = 30.5;
 
         $isMale = false;
 
         echo 4.57;
+        echo "<br>";
         echo $phrase;
 
-
-        
-
-
     ?>
+    <br>
+    <br>
+
+
+
+    <?php
+        // Array
+        $friends = array("Hreiðar", "Gunnar", "Pétur");
+        $friends[1] = "NýttValue";
+
+        // Accessing and displaying array elements
+        foreach ($friends as $friend) {
+            echo "<b> $friend . </b><br>";
+        }
+
+        // Counting the number of elements in the array
+        echo "Number of friends: " . count($friends);
+    ?>
+
+
+
+
+    <br>
+    <br>
+    <!-- post veldur því að upplýsingarnar koma ekki í urlinu -->
+    <form action="basics.php" method="post">
+        password: <input type="password" name="password" value="<?php echo $password; ?>"><br>
+        <input type="submit">
+    </form>
+    <br>
+    <br>
+
+    <!-- sæki upplýsingarnar með post líka -->
+    <?php if(isset($password)): ?>
+    <br>
+    <?php echo $_POST["password"] ?>
+    <?php endif; ?>
 
 
 
--- Coding/php/www/user_input.php
+++ Coding/php/www/user_input.php
@@ -18,6 +18,45 @@
 -->
 
 
+<!-- Checkboxes -->
+<form action="user_input.php" method="post">
+    Apples: <input type="checkbox" name="fruits[]" value="apples"><br>
+    Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>
+    Pears: <input type="checkbox" name="fruits[]" value="pears"><br>
+    <input type="submit">
+</form>
+
+<!-- Get the values of the checkboxes - multiple pieces of information -->
+<?php
+if (isset($_POST["fruits"])) {
+    $fruits = $_POST["fruits"];
+    foreach ($fruits as $fruit) {
+        echo $fruit . "<br>";
+    }
+}
+?>
+
+
+<!-- Associative Arrays -->
+<br>
+Student:
+<form action="user_input.php" method="post">
+    <input type="text" name="student">
+    <input type="submit">
+</form>
+
+<?php
+    $grades = array("Jim"=>"A+", "Hreiðar"=>"A++", "Pétur"=>"B+");
+    // echo $grades[$_POST["student"]];
+?>
+<?php
+    if (isset($_POST["student"])) {
+        echo $_POST['student']; echo ": ";
+        $student = $grades[$_POST["student"]];
+        echo $student;
+
+    }
+?>
 
 
 <form action="user_input.php" method="get">
